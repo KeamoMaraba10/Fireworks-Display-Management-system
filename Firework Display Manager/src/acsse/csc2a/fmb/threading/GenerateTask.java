9e058e4b9a85947a64c520704c141cb9eafc6486
KeamoMaraba10 <keamogetswemaraba.pretoria@gmail.com>
2024-11-20 20:12:15 +0200

    Initail commit

---
+++ Firework Display Manager/src/acsse/csc2a/fmb/threading/GenerateTask.java
@@ -0,0 +1,19 @@
+package acsse.csc2a.fmb.threading;
+
+import acsse.csc2a.fmb.generator.E_TASK_SIZE;
+
+public class GenerateTask implements Runnable{
+	
+	
+	public GenerateTask(E_TASK_SIZE task ,String dataRoot) {
+		
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
